7142e861cda27b1d7013f2e11fb86a720299a22d
mingqing <mingqing@henji.org>
2023-02-09 17:00:02 +0800

    feat: 支持 gRPC Health Checking Protocol 健康检测

--- template/service/handler.go
+++ template/service/handler.go
@@ -142,13 +142,16 @@ import (
 	"context"
 	"net/http"
 
-	"{{ .Global.Repository }}/public/doc"
 	pb "{{ .Global.Repository }}/api/{{ .Global.ProductCode }}/{{ .Global.ShortName }}/{{ .Template.Service.APIVersion }}"
+	"{{ .Global.Repository }}/public/doc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
 // Register 用于服务启动前环境准备
 func (m *Microservice) Register(ctx context.Context) error {
 	pb.Register{{ title .Global.ProductCode }}{{ title .Global.ShortName }}Server(m.server.Server(), m)
+	grpc_health_v1.RegisterHealthServer(m.server.Server(), health.NewServer())
 
 	// 注册服务信息
 	mux, err := m.baseCfg.Register(ctx, pb.Register{{ title .Global.ProductCode }}{{ title .Global.ShortName }}HandlerFromEndpoint)
@@ -157,7 +160,7 @@ func (m *Microservice) Register(ctx context.Context) error {
 	}
 
 	// 注册API文档
-    mux.Handle("/openapi-spec/", http.FileServer(http.FS(doc.Assets)))
+	mux.Handle("/openapi-spec/", http.FileServer(http.FS(doc.Assets)))
 
 	// 这里添加其他自定义实现
 	m.privateHTTPHandle(mux)
