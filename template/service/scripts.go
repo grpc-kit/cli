148872ca794dc8a3f26ae84e936b22c943de0a55
mingqing <mingqing@henji.org>
2023-03-13 16:19:38 +0800

    fix: 更新 openapiv2.yaml 文件的版本号脚本未模版化

--- template/service/scripts.go
+++ template/service/scripts.go
@@ -209,11 +209,11 @@ function update() {
   fi
 
   if test ${GOHOSTOS} = "darwin"; then
-    sed -i "" "s#version: \"${PREFIX_VERSION}\"#version: \"${RELEASE_VERSION}\"#g" api/opsaid/test1/${API_VERSION}/microservice.openapiv2.yaml
+    sed -i "" "s#version: \"${PREFIX_VERSION}\"#version: \"${RELEASE_VERSION}\"#g" api/{{ .Global.ProductCode }}/{{ .Global.ShortName }}/${API_VERSION}/microservice.openapiv2.yaml
   else
     # fix run in container
     # sed: couldn't open temporary file sed1DDoX9: Permission denied
-    #sed -i "s#version: \"${PREFIX_VERSION}\"#version: \"${RELEASE_VERSION}\"#g" api/opsaid/test1/${API_VERSION}/microservice.openapiv2.yaml
+    #sed -i "s#version: \"${PREFIX_VERSION}\"#version: \"${RELEASE_VERSION}\"#g" api/{{ .Global.ProductCode }}/{{ .Global.ShortName }}/${API_VERSION}/microservice.openapiv2.yaml
     cp api/opsaid/test1/${API_VERSION}/microservice.openapiv2.yaml /tmp/microservice.openapiv2.yaml
     sed -i "s#version: \"${PREFIX_VERSION}\"#version: \"${RELEASE_VERSION}\"#g" /tmp/microservice.openapiv2.yaml
     mv /tmp/microservice.openapiv2.yaml api/opsaid/test1/${API_VERSION}/microservice.openapiv2.yaml > /dev/null 2>&1
