5f70441c89846447a39636476ffeb32e6675dc4f
mingqing <mingqing@henji.org>
2023-12-26 10:27:58 +0800

    build: make manifests TEMPLATES example

--- template/service/root.go
+++ template/service/root.go
@@ -66,7 +66,7 @@ go 1.20
 
 require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.18.1
-	github.com/grpc-kit/pkg v0.3.4
+	github.com/grpc-kit/pkg v0.3.5
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.17.0
@@ -156,7 +156,7 @@ precheck: ## Check environment.
 ##@ Development
 
 .PHONY: generate
-manifests: ## Generate deployment manifests files.
+manifests: ## Generate deployment manifests files, like: "make manifests TEMPLATES=kubernetes".
 	@./scripts/manifests.sh
 
 generate: precheck ## Generate code from proto files.
